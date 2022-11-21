f28248f2ce2a7144cfd5733461287ec6a200023f
pearson-png <kp1@wellesley.edu>
2022-11-21 16:17:34 -0500

    added find_users() and username_from_uid()

--- draft/queries.py
+++ draft/queries.py
@@ -22,3 +22,19 @@ def find_courses(conn):
     curs.execute('''
         select courseid, title, code from courses''')
     return curs.fetchall()
+
+
+def find_users(conn):
+    '''Returns a list of dictionaries of all users'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+        select uid, username from users''')
+    return curs.fetchall()
+
+
+def username_from_uid(conn, uid):
+    '''Returns the current username associated with the give uid'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+        select username from users where uid = %s''', [uid])
+    return curs.fetchone()
