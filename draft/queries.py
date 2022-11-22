6e14c2e7322557751136211b28dac706c171ecd8
Remi Fernandez <rf101@wellesley.edu>
2022-11-22 13:40:27 -0500

    new view_post.html page, links from home page

--- draft/queries.py
+++ draft/queries.py
@@ -32,3 +32,61 @@ def recent_posts(conn):
         order by time desc
         limit 50''')
     return curs.fetchall()
+
+
+def post_user(conn, postid):
+    '''Returns username that wrote a specific post '''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+    select users.username
+    from posts inner join users 
+    on (posts.user = users.uid)
+    where postid = %s''', [postid])
+    return curs.fetchone()
+
+def post_course_info(conn, postid):
+    '''Returns course rating and course code for single course from a specific post '''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+    select posts.course_rating, courses.code
+    from posts inner join courses 
+    on (posts.course = courses.courseID)
+    where postid = %s''',[postid]) 
+    #returns course rating and course code
+    return curs.fetchall()
+
+def post_prof_info(conn, postid):
+    '''Returns rating and name for single prof from a specific post '''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+    select posts.prof_rating, professors.name
+    from posts inner join professors 
+    on (posts.prof = professors.pid)
+    where postid = %s''', [postid]) 
+    #returns prof rating and prof name
+    return curs.fetchall()
+
+def post_text(conn, postid):
+    '''Returns text from a specific post '''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+    select posts.text
+    from posts 
+    where postid = %s''', [postid]) 
+    #returns text from one post
+    return curs.fetchone()
+
+def post_time(conn, postid):
+    '''Returns timestamp from a specific post '''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+    select posts.time
+    from posts 
+    where postid = %s''', [postid]) 
+    #returns time from one post
+    return curs.fetchone()
+
+#what do I do if one of my queries returns multiple values (ex. prof rating and prof name)? How can I later separate those?
+# is it better to write 2 different queries, one for each value/piece of info? 
+#( basically just copy and paste current queries and change what they return)
+
