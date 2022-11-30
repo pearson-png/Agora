118f9a8e7c2db1413b9bc84263840ef4044283ca
pearson-png <kp1@wellesley.edu>
2022-11-30 13:55:29 -0500

    copying contests to alpha folder and edoted post form for set profs and courses

---
+++ alpha/app.py
@@ -0,0 +1,211 @@
+from flask import (Flask, render_template, make_response, url_for, request,
+                   redirect, flash, session, send_from_directory, jsonify)
+from werkzeug.utils import secure_filename
+app = Flask(__name__)
+
+# one or the other of these. Defaults to MySQL (PyMySQL)
+# change comment characters to switch to SQLite
+
+import cs304dbi as dbi
+# import cs304dbi_sqlite3 as dbi
+
+import random
+import queries
+from datetime import datetime
+
+app.secret_key = 'your secret here'
+# replace that with a random key
+app.secret_key = ''.join([ random.choice(('ABCDEFGHIJKLMNOPQRSTUVXYZ' +
+                                          'abcdefghijklmnopqrstuvxyz' +
+                                          '0123456789'))
+                           for i in range(20) ])
+
+# This gets us better error messages for certain common request errors
+app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    conn = dbi.connect()
+    if request.method == 'GET':
+        #gets homepage with dropdown menu of all choices and recent posts
+        departments = queries.find_depts(conn)
+        professors = queries.find_profs(conn)
+        courses = queries.find_courses(conn)
+        posts = queries.recent_posts(conn)
+        return render_template('home_page.html',title='Hello', 
+        departments = departments, courses = courses, 
+        professors = professors, posts = posts)
+    if request.method == 'POST':
+        #redirects according to filter options to dept/prof/course route
+        filters = request.form
+        dept = filters['department']
+        prof = filters['professor']
+        course = filters['course']
+        ##options here
+        if dept == "0":
+            #if no dept chosen, reload the homepage no matter other fields
+            departments = queries.find_depts(conn)
+            professors = queries.find_profs(conn)
+            courses = queries.find_courses(conn)
+            posts = queries.recent_posts(conn)
+            flash("Please choose a department")
+            return render_template('home_page.html',title='Hello', 
+            departments = departments, courses = courses, 
+            professors = professors, posts = posts)
+        elif prof == "0" and course == "0":
+            #if only department chosen, direct to dept page
+            return redirect(url_for('department', department=dept))
+        elif course == "0" and prof != "0":
+            #if only prof chosen, direct to prof page
+            return redirect(url_for('professor', department=dept, professor=prof))
+        elif course != "0" and prof == "0":
+            #if only course chosen, direct to course page
+            return redirect(url_for('course', department=dept, course=course))
+        else:
+            #if all three were chosen, give specific prof and course page
+            return redirect(url_for('course_section', department=dept, professor=prof, course=course))
+
+
+@app.route('/view/<postid>', methods=['GET','POST'])
+#view individual posts
+def view_post(postid):
+    conn = dbi.connect()
+    # if user clicks on a post
+    if request.method == 'GET':
+        user = queries.post_user(conn, postid)['username']
+        course_code = queries.post_course_code(conn, postid)['code'] 
+        course_rating = queries.post_course_rating(conn, postid)['course_rating']
+        prof_name = queries.post_prof_name(conn, postid)['name']
+        prof_rating = queries.post_prof_rating(conn, postid)['prof_rating']
+        text = queries.post_text(conn,postid)['text']
+        time = queries.post_time(conn,postid)['time']
+
+    return render_template('view_post.html', action= url_for('view_post', postid=postid), 
+        user=user, course_code=course_code, course_rating=course_rating, prof_name=prof_name, prof_rating=prof_rating, text=text, time=time) 
+        
+@app.route('/upload/', methods=['GET','POST'])
+def upload():
+    conn = dbi.connect()
+    ### TO DO: Check if user is logged in and get their user id
+    # for now assign random user
+    people = queries.find_users(conn)
+    uid = random.choice([person['uid'] for person in people])
+    professors = queries.find_profs(conn)
+    courses = queries.find_courses(conn)
+    departments = queries.find_depts(conn)
+
+    if request.method == 'GET':
+        # load form
+        return render_template('post-form.html', title='Create a Post',
+        professors = professors, courses = courses, departments = departments)
+    else: # method == POST
+        # retrieve information
+        username = queries.username_from_uid(conn, uid)
+        time = datetime.now()
+        dept = request.form['dept']
+        profid = request.form['pid']
+        courseid = request.form['course']
+        prof_rating = request.form['prof-rating']
+        course_rating = request.form['course-rating']
+        review_text = request.form['review-text']
+
+        ### TO DO: add pdf table to database; get pdf from form and append to database
+        pdf = request.files['pdf']
+        print(type(pdf))
+
+        # check if essential information is present
+        if dept == "":
+            flash('Please choose a department')
+            return render_template('post-form.html', title='Create a Post',
+            professors = professors, courses = courses, departments = departments)
+        if profid == "" and courseid == "":
+            flash('Please enter a Professor and/or Course.')
+            return render_template('post-form.html', title='Create a Post',
+            professors = professors, courses = courses, departments = departments)
+        elif profid == "" and prof_rating != "":
+            flash('Please provide a Professor to rate or remove the \
+                    Professor Rating.')
+            return render_template('post-form.html', title='Create a Post',
+            professors = professors, courses = courses, departments = departments)
+        elif courseid == "" and course_rating != "":
+            flash('Please provide a Course to rate or remove the \
+                    Course Rating.')
+            return render_template('post-form.html', title='Create a Post',
+                                    professors = professors, courses = courses, 
+                                    departments = departments)
+        elif (prof_rating == "" and course_rating == "" and review_text 
+        == "" and not request.files.get('pdf', None)):
+            flash('Please provide content for your post.')
+            return render_template('post-form.html', title='Create a Post',
+                                    professors = professors, courses = courses,
+                                    departments = departments)
+        
+        ### use code below if we don't get the course browser csv and have user enter info manually
+        # new_dept_name = request.form['new-dept-name']
+        # new_dept_abbrv = request.form['new-dept-name']
+        # new_prof_name = request.form['new-prof-name']
+        # new_course_name = request.form['new-course-name']
+        # new_course_code = request.form['new-course-code']
+        # raw_info = [dept, new_dept_name, new_dept_abbrv, profid, new_prof_name, courseid,\
+        # new_course_name, new_course_code, prof_rating, course_rating, review_text]
+
+        # # if entering a new department
+        # if dept == 'other':
+        #     if (new_dept_name == "" or new_dept_abbrv == ""):
+        #         flash('Please enter the required information to add a new \
+        #         department, or select one from the drop down menu above.')
+        #         return render_template('post-form.html', title='Create a Post',
+        #                                 professors = professors, courses = courses, 
+        #                                 departments = departments)
+        #     queries.add_department(conn, new_dept_name, new_dept_abbrv)
+        #     dept = new_dept_abbrv
+
+        # # if entering a new professor
+        # if profid == 'other':
+        #     if (new_prof_name == ""):
+        #         flash('Please enter the required information to add a new \
+        #         professor, or select one from the drop down menu above.')
+        #         return render_template('post-form.html', title='Create a Post',
+        #                                 professors = professors, courses = courses, 
+        #                                 departments = departments)
+        #     profid = queries.add_professor(conn, new_prof_name, dept)
+        
+        # # if entering a new course
+        # if courseid == 'other':
+        #     if (new_course_name == "" or new_course_code == ""):
+        #         flash('Please enter the required information to add a new \
+        #         course, or select one from the drop down menu above.')
+        #         return render_template('post-form.html', title='Create a Post',
+        #                                 professors = professors, courses = courses, 
+        #                                 departments = departments)
+        #     courseid = queries.add_course(conn, new_course_name, new_course_code, dept)
+
+        # upload post
+        if profid == '':
+            postid = queries.add_course_post(conn, time, uid, courseid, course_rating, review_text, pdf)
+        elif courseid == '':
+            postid = queries.add_prof_post(conn, time, uid, profid, prof_rating, review_text, pdf)
+        else:
+            postid = queries.add_post(conn, time, uid, courseid, profid, prof_rating, course_rating, review_text, pdf)
+        flash('Upload successful')
+        # go to post page
+        return redirect(url_for('view_post', postid=postid['last_insert_id()']))
+
+@app.before_first_request
+def init_db():
+    dbi.cache_cnf()
+    # set this local variable to 'wmdb' or your personal or team db
+    db_to_use = 'agora_db' 
+    dbi.use(db_to_use)
+    print('will connect to {}'.format(db_to_use))
+
+if __name__ == '__main__':
+    import sys, os
+    if len(sys.argv) > 1:
+        # arg, if any, is the desired port number
+        port = int(sys.argv[1])
+        assert(port>1024)
+    else:
+        port = os.getuid()
+    app.debug = True
+    app.run('0.0.0.0',port)
---
+++ alpha/queries.py
@@ -0,0 +1,184 @@
+import cs304dbi as dbi
+
+def find_depts(conn):
+    '''Returns a list of dictionaries of all departments'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+        select * from departments''')
+    return curs.fetchall()
+
+
+def find_profs(conn):
+    '''Returns a list of dictionaries of all professors'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+        select pid, name from professors''')
+    return curs.fetchall()
+
+
+def find_courses(conn):
+    '''Returns a list of dictionaries of all courses'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+        select courseid, title, code from courses''')
+    return curs.fetchall()
+
+def recent_posts(conn):
+    '''Returns a list of dictionaries of 50 most recent posts'''
+    '''IMPORTANT note: switch this to be with infinite scrolling later'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+        select * from posts
+        order by time desc
+        limit 50''')
+    return curs.fetchall()
+
+
+def post_user(conn, postid):
+    '''Returns username that wrote a specific post '''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+    select users.username
+    from posts inner join users 
+    on (posts.user = users.uid)
+    where postid = %s''', [postid])
+    return curs.fetchone()
+
+def post_course_rating(conn, postid):
+    '''Returns course rating  single course from a specific post '''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+    select posts.course_rating
+    from posts inner join courses 
+    on (posts.course = courses.courseID)
+    where postid = %s''',[postid]) 
+    #returns course rating and course code
+    return curs.fetchone()
+
+def post_course_code(conn, postid):
+    '''Returns course code for single course from a specific post '''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+    select courses.code
+    from posts inner join courses 
+    on (posts.course = courses.courseID)
+    where postid = %s''',[postid]) 
+    #returns course rating and course code
+    return curs.fetchone()
+
+def post_prof_rating(conn, postid):
+    '''Returns rating single prof from a specific post '''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+    select posts.prof_rating
+    from posts inner join professors 
+    on (posts.prof = professors.pid)
+    where postid = %s''', [postid]) 
+    #returns prof rating and prof name
+    return curs.fetchone()
+
+def post_prof_name(conn, postid):
+    '''Returns name for single prof from a specific post '''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+    select professors.name
+    from posts inner join professors 
+    on (posts.prof = professors.pid)
+    where postid = %s''', [postid]) 
+    #returns prof rating and prof name
+    return curs.fetchone()
+
+def post_text(conn, postid):
+    '''Returns text from a specific post '''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+    select posts.text
+    from posts 
+    where postid = %s''', [postid]) 
+    #returns text from one post
+    return curs.fetchone()
+
+def post_time(conn, postid):
+    '''Returns timestamp from a specific post '''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+    select posts.time
+    from posts 
+    where postid = %s''', [postid]) 
+    #returns time from one post
+    return curs.fetchone()
+
+def find_users(conn):
+    '''Returns a list of dictionaries of all users'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+        select uid, username from users''')
+    return curs.fetchall()
+
+def username_from_uid(conn, uid):
+    '''Returns the current username associated with the give uid'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+        select username from users where uid = %s''', [uid])
+    return curs.fetchone()
+
+def add_post(conn, time, user, course, prof, prof_rating, course_rating, text, attachments):
+    '''Add a new post about course and professor to the database and returns the postid'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''insert into posts(time, user, course, prof, prof_rating, course_rating, text, attachments) 
+    values(%s,%s,%s,%s,%s,%s,%s,%s)''',
+    [time, user, course, prof, prof_rating, course_rating, text, attachments])
+    conn.commit()
+    curs.execute('''select last_insert_id() from posts''')
+    return curs.fetchone() 
+
+def add_course_post(conn, time, user, course, course_rating, text, attachments):
+    '''Add a new post about a course to the database and returns the postid'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''insert into posts(time, user, course, course_rating, text, attachments) 
+    values(%s,%s,%s,%s,%s,%s)''',
+    [time, user, course, course_rating, text, attachments])
+    conn.commit()
+    curs.execute('''select last_insert_id() from posts''')
+    return curs.fetchone()
+
+def add_prof_post(conn, time, user, prof, prof_rating, text, attachments):
+    '''Add a new post about a professor to the database and returns the postid'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''insert into posts(time, user, prof, prof_rating, text, attachments) 
+    values(%s,%s,%s,%s,%s,%s)''',
+    [time, user, prof, prof_rating, text, attachments])
+    conn.commit()
+    curs.execute('''select last_insert_id() from posts''')
+    return curs.fetchone() 
+
+# use code below if we don't get the course browser csv
+
+# def add_professor(conn, name, dept):
+#     '''Adds a professor to the database and returns pid'''
+#     # add prof
+#     curs = dbi.dict_cursor(conn)
+#     curs.execute('''
+#     insert into professors(name, dept) values(%s,%s)''',[name, dept])
+#     conn.commit()
+#     # get pid
+#     curs.execute('''
+#     select last_insert_id() from professors''')
+#     return curs.fetchone()    
+
+# def add_course(conn, name, code, dept):
+#     '''Adds a course to the database and returns courseid'''
+#     curs = dbi.dict_cursor(conn)
+#     curs.execute('''
+#     insert into courses(title, code, dept) values(%s,%s,%s)''',[name, code, dept])
+#     conn.commit()
+#     curs.execute('''
+#     select last_insert_id() from courses''')
+#     return curs.fetchone()  
+
+# def add_department(conn, name, abbrv):
+#     '''Add a department to the database'''
+#     curs = dbi.dict_cursor(conn)
+#     curs.execute('''
+#     insert into departments(name, abbrv) values(%s,%s)''',[name, abbrv])
+#     conn.commit()
